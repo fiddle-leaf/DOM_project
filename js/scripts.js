bb2d559c39eee4e4c19a52667a304db49030faf7
fiddle.leaf <shanonps@icloud.com>
2023-07-02 14:17:40 -0400

    Added buttons for light/dark mode, reset, and submit

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+//grab container element
+const containerEL = document.getElementById("container")
+//console.log(containerEL);
+
+/**
+ * ACTORS: User factory gets User input
+ */
+
+/**
+ * ACTIONS: makeOutfit(), disable(), changeTheme()
+ */
+
+const changeTheme = (e) => {
+    e.classList.add();
+}
