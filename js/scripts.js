284285a80b39ca5b4b625097324dee67103f5047
fiddle.leaf <shanonps@icloud.com>
2023-07-03 22:37:40 -0400

    Update light/dark buttons to switch on and off light and dark mode

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,54 @@
 //grab container element
-const containerEL = document.getElementById("container")
-//console.log(containerEL);
+const bodyEl = document.querySelector("body");
+//console.log(bodyEL);
 
 /**
  * ACTORS: User factory gets User input
  */
-
+const User = (name) => {
+    
+}
 /**
  * ACTIONS: makeOutfit(), disable(), changeTheme()
  */
 
-const changeTheme = (e) => {
-    e.classList.add();
+const lightMode = bodyEl.querySelector("#light-mode");
+const darkMode = bodyEl.querySelector("#dark-mode");
+
+console.log(lightMode);
+console.log(darkMode);
+
+const changeTheme = (mode) => {
+
+    if (mode == "light") {
+        bodyEl.classList.contains("dark") ?
+        (bodyEl.classList.remove("dark"),
+        bodyEl.classList.add("light") ):
+        bodyEl.classList.add("light");
+    } else if (mode == "dark") {
+        bodyEl.classList.contains("light") ?
+        (bodyEl.classList.remove("light"), 
+        bodyEl.classList.add("dark")) :
+        bodyEl.classList.add("dark");
+    }
 }
+
+lightMode.addEventListener("click", (event) => {
+    event.preventDefault;
+
+    if (event.target.tagName.toLowerCase() != "button") {
+        return;
+    } else {
+        changeTheme("light");
+    }
+});
+
+darkMode.addEventListener("click", (event) => {
+    event.preventDefault;
+
+    if (event.target.tagName.toLowerCase() != "button") {
+        return;
+    } else {
+        changeTheme("dark");
+    }
+});
