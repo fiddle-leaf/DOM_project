e662e9329ca15deb13fd04efae1fab4ac72a14a6
sPalominoSal <82245130+fiddle-leaf@users.noreply.github.com>
2023-07-07 22:39:10 -0400

    Add functionality to light/dark mode according to color selected by user

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,67 @@ const bodyEl = document.querySelector("body");
 //console.log(bodyEL);
 
 /**
- * ACTORS: User factory gets User input
+ * ACTORS: User factory saves User input
  */
-const User = (name) => {
-    
+let uname = "", mode = "", color = "";
+
+const User = () => {
+    return Object.assign(
+        {uname,
+        mode,
+        color}
+    )
+}
+
+const colorHues = {
+    red: 355,
+    orange: 22,
+    yellow: 40,
+    green: 110,
+    blue: 200,
+    purple: 280,
+    pink: 320,
+    black: 0
 }
 /**
- * ACTIONS: makeOutfit(), disable(), changeTheme()
+ * ACTIONS: createUser(), colorTheme(), & makeOutfit()
  */
+
+const colorHSL = (h, s, l) => {
+    return `hsl(${colorHues[h]}, ${s}%, ${l}%)`;
+}
+
+const colorTheme = (userColor) => {
+    let sat = 100, light = 95, dark = 30, background, color;
+    console.log(mode);
+    switch(mode) {
+        case "light": {
+            userColor ?
+            userColor != "black" ?
+            (background = colorHSL(userColor, sat, light),
+            color = colorHSL(userColor, sat, dark)) :
+            (background = colorHSL(userColor, 0, 98), 
+            color = "#000") :
+            (background = "#fff", color = "#000");
+            break;
+        }
+
+        case "dark": {
+            userColor ?
+            userColor != "black" ?
+            (background = colorHSL(userColor, sat, dark),
+            color = colorHSL(userColor, sat, light)) :
+            (background = colorHSL(userColor, 0, 2),
+            color = "#fff") :
+            (background = "#000", color = "#fff");
+            break;
+        }
+    }
+    return Object.assign(bodyEl.style,
+        {background,
+        color}
+    )
+}
 
 const lightMode = bodyEl.querySelector("#light-mode");
 const darkMode = bodyEl.querySelector("#dark-mode");
@@ -18,62 +71,35 @@ const darkMode = bodyEl.querySelector("#dark-mode");
 //console.log(lightMode);
 //console.log(darkMode);
 
-const changeTheme = (mode) => {
-
-    switch (mode) {
-        case "light": {
-        bodyEl.classList.contains("dark") ?
-        (bodyEl.classList.remove("dark"),
-        bodyEl.classList.add(mode) ):
-        bodyEl.classList.add(mode);
-        break;
-    };
-        case "dark": {
-        bodyEl.classList.contains("light") ?
-        (bodyEl.classList.remove("light"), 
-        bodyEl.classList.add(mode)) :
-        bodyEl.classList.add(mode);
-        break;
-    };
-}
-}
-
 /**
  * changing between dark/light modes
  */
-lightMode.addEventListener("click", (event) => {
-    event.preventDefault;
-
-    if (event.target.tagName.toLowerCase() != "button") {
-        return;
-    } else {
-        changeTheme("light");
-    }
+lightMode.addEventListener("click", (e) => {
+    e.preventDefault();
+    mode = "light";
+    colorTheme();
 });
 
-darkMode.addEventListener("click", (event) => {
-    event.preventDefault;
+darkMode.addEventListener("click", (e)=>{
+    e.preventDefault();
+    mode = "dark";
+    colorTheme();
+})
 
-    if (event.target.tagName.toLowerCase() != "button") {
-        return;
-    } else {
-        changeTheme("dark");
-    }
-});
+const colorPick = bodyEl.querySelectorAll("input[name='color']");
+//console.log(colorPick);
 
-/**
- * Class for Layout colors
- */
-let colorPick = []
-colorPick = bodyEl.querySelector("#color-radios").getElementsByClassName("grid");
-
-console.log(colorPick);
-
-for (color in colorPick) {
-    console.log(color);
-}
+colorPick.forEach((colorEl) => {
+    console.log(colorEl);
 
 
-const colorTheme = (color) => {
-    return Object.assign
-}
+    colorEl.addEventListener("click", (event)=>{
+        const clickedColor = event.target
+        if (clickedColor.tagName.toLowerCase() != "input"){
+            return;
+        } else {
+            clickedColor.setAttribute("checked", "");
+            colorTheme(clickedColor.id);
+        }
+    })
+})
