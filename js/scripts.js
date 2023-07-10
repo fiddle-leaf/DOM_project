3b20b2a3458a0245db0357ff600df11ec6a8de70
fiddle.leaf <shanonps@icloud.com>
2023-07-10 10:59:48 -0400

    Fixed issue with color modes, add output from selection

--- js/scripts.js
+++ js/scripts.js
@@ -1,121 +1,5 @@
 //grab container element
 const bodyEl = document.querySelector("body");
-//console.log(bodyEL);
-
-/**
- * ACTORS: User factory saves User input
- */
-let uname = "", mode = "light", color = "";
-
-const User = () => {
-    return Object.assign(
-        {uname,
-        mode,
-        color}
-    )
-}
-
-const colorHues = {
-    red: 355,
-    orange: 22,
-    yellow: 40,
-    green: 110,
-    blue: 200,
-    purple: 280,
-    pink: 325,
-    black: 0
-}
-
-const outfitItems = {
-    tops: [
-        {tank: 'tank-top.png'},
-        {shortSleeve: 'short-sleeve.png'},
-        {longSleeve: 'long-sleeve.png'},
-        {dress: 'dress.png'}
-    ],
-    bottoms: [
-        'skirt.png',
-        'shorts.png',
-        'pants.png'
-    ],
-    shoes: [
-        'sneakers.png',
-        'sandals.png',
-        'boots.png'
-    ],
-    accs: [
-        'earrings.png',
-        'belt.png',
-        'purse.png'
-    ]
-}
-/**
- * ACTIONS: createUser(), colorTheme(), & makeOutfit()
- */
-
-const colorHSL = (h, s, l) => {
-    return `hsl(${colorHues[h]}, ${s}%, ${l}%)`;
-}
-
-const makeImgs = (imgSrc, item) => {
-    const folder = './imgs/';
-    const img = new Image();
-    img.src = folder + imgSrc;
-    img.alt = item;
-
-    return img;
-}
-
-const colorTheme = (userColor) => {
-    let sat = 100, light = 95, dark = 30, background, color;
-    //console.log(mode);
-    
-    switch(mode) {
-        case "light": {
-            userColor ?
-            userColor != "black" ?
-            (background = colorHSL(userColor, sat, light),
-            color = colorHSL(userColor, sat, dark)) :
-            (background = colorHSL(userColor, 0, 98), 
-            color = "#000") :
-            (background = "#fff", color = "#000");
-            break;
-        }
-
-        case "dark": {
-            userColor ?
-            userColor != "black" ?
-            (background = colorHSL(userColor, sat, dark),
-            color = colorHSL(userColor, sat, light)) :
-            (background = colorHSL(userColor, 0, 2),
-            color = "#fff") :
-            (background = "#000", color = "#fff");
-            break;
-        }
-    }
-    return Object.assign(bodyEl.style,
-        {background,
-        color}
-    )
-}
-
-const displayItem = (el) => {
-    el.forEach(item => {
-        item.addEventListener("click", (event) =>{
-
-            if (event.target.tagName.toLowerCase() != "input"){
-                return;
-            } else {
-                event.target.setAttribute("checked", "");
-                const item = event.target.id;
-                let itemImg;
-                console.log(item);
-                outputEl.innerHTML = "";
-            }
-        })
-    })
-}
-
 // elements from html page
 const colorPick = bodyEl.querySelectorAll("input[name='color']");
 const lightMode = bodyEl.querySelector("#light-mode");
@@ -124,40 +8,218 @@ const outputEl = bodyEl.querySelector(".output");
 const topsEL = bodyEl.querySelectorAll("input[name='tops']");
 const bottomsEl = bodyEl.querySelectorAll("input[name='bottoms']");
 const shoesEl = bodyEl.querySelectorAll("input[name='shoes']");
-//console.log(lightMode);
-//console.log(darkMode);
-//console.log(colorPick);
+const accsEl = bodyEl.querySelectorAll("input[name='accs']");
+const saveEl = bodyEl.querySelector("button[type='submit']");
+let outfitOutput = document.createElement("div");
+outfitOutput.id = "options";
 
-colorPick.forEach((colorEl) => {
-    //console.log(colorEl);
+//console.log(bodyEL);
+/**
+ * Global variables: uname for user, users list, and page "mode";
+ */
+let uname = "", mode = "light-mode", userColor;
+const users = [];
+const userOutfit = [];
 
-    colorEl.addEventListener("click", (event)=>{
-        const clickedColor = event.target;
-        if (clickedColor.tagName.toLowerCase() != "input"){
-            return;
-        } else {
-            clickedColor.setAttribute("checked", "");
-            color = clickedColor.id;
-            colorTheme(color);
-        }
+/**
+ * ACTORS: User factory saves User input
+ */
+
+class User {
+    constructor(name){
+        this.name  = name;
+    }
+
+    displayName = () => {
+        let userHead = document.createElement("h3");
+        userHead.textContent = `${this.name}'s OOTD`;
+        outputEl.appendChild(userHead);
+    }
+
+    displayOutfit = (outfitArray) => {
+        outputEl.appendChild(outfitOutput);
+
+        outfitArray.forEach(item => {
+            Object.values(item).every(item => {
+                item.style.background = "#fff";
+                outfitOutput.appendChild(item);
+            });
+        })
+    }
+
+}
+
+const colorHues = {
+    red: 355,
+    orange: 20,
+    yellow: 40,
+    green: 120,
+    blue: 210,
+    purple: 280,
+    pink: 325
+}
+
+
+/**
+ * ACTIONS: createUser(), colorTheme(), & makeOutfit()
+ */
+
+const colorHSL = (h, s, l) => {
+    if (h == "black") {
+        return `hsl(0, 0%, ${l}%)`;
+    } else {
+    return `hsl(${colorHues[h]}, ${s}%, ${l}%)`;
+    }
+}
+
+const createUser = () => {
+    uname = bodyEl.querySelector("#name").value;
+    uname.length > 0 ?
+    users.push(new User(uname)):
+    false;
+}
+
+const verifyInput = (target) => {
+    if (target.tagName.toLowerCase() != "input") {
+        return;
+    } else {
+        return target.id;
+    }
+}
+
+const setChecked = (el) => {
+    el.setAttribute("checked", "");
+}
+
+const setMode = (el) => {
+    el.addEventListener("click", e => {
+        e.preventDefault();
+
+        mode = e.target.id;
+        //console.log(mode);
+        setColorTheme();
+    })
+}
+
+const setColorTheme = () => {
+    let background, color
+    sat = 100, light = 95, dark = 30;
+    //console.log(mode, userColor);
+
+    switch(mode) {
+        case "light-mode":
+            userColor === undefined ?
+            (background = "#fff", color = "#000") :
+            (background = colorHSL(userColor, sat, light),
+            color = colorHSL(userColor, sat, dark));
+            break;
+        
+        case "dark-mode":
+            userColor === undefined ?
+            (background = "#000", color = "#fff") :
+            (background = colorHSL(userColor, sat, dark),
+            color = colorHSL(userColor, sat, light));
+            break;
+    }
+
+    return Object.assign(bodyEl.style,
+        {background, 
+        color})
+}
+
+const makeImgs = (item) => {
+    let imgSrc = item+".png";
+    const folder = './imgs/';
+    const img = new Image();
+    img.src = folder + imgSrc;
+    img.alt = item;
+
+    return img;
+}
+
+const saveOutfit = (item, itemImg) => {
+    let outfitObj  = {
+        [item] : itemImg
+    };
+    userOutfit.push(outfitObj);
+}
+
+/**
+ * EVENT LISTENERS
+ */
+
+setMode(lightMode);
+setMode(darkMode);
+
+colorPick.forEach(colorItem => {
+    colorItem.addEventListener("click", e => {
+        userColor = verifyInput(e.target);
+        //console.log(userColor);
+        setColorTheme();
     })
 })
 
-displayItem(topsEL);
-displayItem(bottomsEl);
-displayItem(shoesEl);
+topsEL.forEach(topItem => {
+    topItem.addEventListener("click", e => {
+        clickedTopItem = verifyInput(topItem);
+        setChecked(e.target);
 
-/**
- * changing between dark/light modes
- */
-lightMode.addEventListener("click", (e) => {
-    e.preventDefault();
-    mode = "light";
-    colorTheme(color);
-});
+        if (clickedTopItem == "dress") {
+            bottomsEl.forEach(bottomItem => {
+                bottomItem.setAttribute("disabled", "");
+            })
+        } else {
+            bottomsEl.forEach(bottomItem => {
+                bottomItem.removeAttribute("disabled", "");
+            })
+        }
 
-darkMode.addEventListener("click", (e)=>{
-    e.preventDefault();
-    mode = "dark";
-    colorTheme(color);
+        let topImg = makeImgs(clickedTopItem);
+        outputEl.innerHTML = "";
+        outputEl.appendChild(topImg);
+        saveOutfit(clickedTopItem, topImg);
+    })
 })
+
+bottomsEl.forEach(bottomItem => {
+    bottomItem.addEventListener("click", e => {
+        clickedBottoms = verifyInput(bottomItem);
+        setChecked(e.target);
+        let bottomImg = makeImgs(clickedBottoms);
+        outputEl.innerHTML = "";
+        outputEl.appendChild(bottomImg);
+        saveOutfit(clickedBottoms, bottomImg);
+    })
+})
+
+shoesEl.forEach(shoesItem => {
+    shoesItem.addEventListener("click", e => {
+        clickedShoes = verifyInput(shoesItem);
+        setChecked(e.target);
+        let shoesImg = makeImgs(clickedShoes);
+        outputEl.innerHTML = "";
+        outputEl.appendChild(shoesImg);
+        saveOutfit(clickedShoes, shoesImg);
+    })
+})
+
+accsEl.forEach(accsItem => {
+    accsItem.addEventListener("click", e => {
+        clickedAccs = verifyInput(accsItem);
+        setChecked(e.target);
+        let accsImg = makeImgs(clickedAccs);
+        outputEl.innerHTML = "";
+        outputEl.appendChild(accsImg);
+        saveOutfit(clickedAccs, accsImg);
+    })
+})
+
+saveEl.addEventListener("click", e => {
+    e.preventDefault();
+    createUser();
+    
+    for (i of users){
+        i.displayName();
+        i.displayOutfit(userOutfit);
+    }
+});
